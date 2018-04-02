74600b867735657644fbb04416cc0d24878b361c
igor1989 <ak@Computer.local>
2018-04-02 14:33:16 -0500

    Add connection to SimplyRets

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -1,14 +1,30 @@
 <?php
 
 namespace App\Http\Controllers;
+use GuzzleHttp\Exception\GuzzleException;
+use GuzzleHttp\Client;
 
 class PagesController extends Controller {
 
-	public function getIndex(){
-		return view('pages.home');
+	public function getIndex()
+	{
+		$client = new \GuzzleHttp\Client;
+
+		$listings = $client->get(
+			'https://api.simplyrets.com/properties', 
+			['auth' => ['simplyrets', 'simplyrets']]
+		);
+
+		// Parse it into an array
+		$listsing = json_decode($listings->getBody(), true);
+
+		var_dump($listings);
+
+		return view('pages.home', $listings);
 	}
 
-	public function getContact(){
+	public function getContact()
+	{
 		return view('pages.contact');
 	}
 }
