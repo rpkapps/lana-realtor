d1a2b717bf866a66b05762fbdf4e7aa2987fcb37
igor1989 <ak@Computer.local>
2018-06-04 21:54:22 -0500

    Add image gallery

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,5 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/assets/js/pages/listings.js', 'public/js')
+mix.js('resources/assets/js/pages/listings.js', 'public/js') 
    .sass('resources/assets/sass/app.scss', 'public/css');
