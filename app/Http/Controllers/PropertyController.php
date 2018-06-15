561011adc04cf806febb6ef9d4c549f55c08fb8a
Ruslan Kolesnik <rpkolesnikg@gmail.com>
2018-06-14 21:30:27 -0400

    Add filter functionality to our PropertyController

--- app/Http/Controllers/PropertyController.php
+++ app/Http/Controllers/PropertyController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Resources\PropertyResource;
 use App\Property;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
 
 class PropertyController extends Controller
@@ -16,23 +17,47 @@ class PropertyController extends Controller
      */
     public function index(Request $request)
     {
+        $columnMap = [
+            'listprice' => 'listPrice',
+            'listdate' => 'created_at',
+            'beds' => 'bedrooms',
+            'baths' => 'bathrooms'
+        ];
+
         $query = Property::query();
 
         // Pagination limit per page
         $limit = $request->query('limit', 9);
+
         // Search query
-        $q = $request->query('q', null);
+        $this->search($query, Property::QUERYABLE_COLUMNS, $request->query('q'));
 
-        // Search
-        if($q) {
-            foreach (Property::QUERYABLE_COLUMNS as $index => $column) {
-                $query->orWhere($column, 'LIKE', '%' . $q . '%');
-            }
-        }
+        // Min Price
+        $this->minFilter($query, 'listPrice', $request->get('minprice'));
 
-        // TODO: filter
+        // Max Price
+        $this->maxFilter($query, 'listPrice', $request->get('maxprice'));
 
-        // TODO: sort
+        // Min Area
+        $this->minFilter($query, 'area', $request->get('minarea'));
+
+        // Max Area
+        $this->maxFilter($query, 'area', $request->get('maxarea'));
+
+        // Min Bedrooms
+        $this->minFilter($query, 'bedrooms', $request->get('minbeds'));
+
+        // Max Bedrooms
+        $this->maxFilter($query, 'bedrooms', $request->get('maxbeds'));
+
+        // Min Bathrooms
+        $this->minFilter($query, 'bathrooms', $request->get('minbaths'));
+
+        // Min Bathrooms
+        $this->maxFilter($query, 'bathrooms', $request->get('maxbaths'));
+
+        // Sort
+        $this->sort($query, $request->get('sort'), $columnMap);
 
         return PropertyResource::collection($query->paginate($limit));
     }
@@ -47,4 +72,71 @@ class PropertyController extends Controller
     {
         return new PropertyResource($property);
     }
+
+    /**
+     * Search filter helper
+     *
+     * @param Builder $query
+     * @param array $columns
+     * @param $value
+     */
+    protected function search(Builder $query, $columns = [], $value)
+    {
+        if ($value) {
+            foreach ($columns as $index => $column) {
+                $query->orWhere($column, 'LIKE', '%' . $value . '%');
+            }
+        }
+    }
+
+    /**
+     * Min filter helper
+     *
+     * @param Builder $query
+     * @param string $column
+     * @param $value
+     */
+    protected function minFilter(Builder $query, $column = '', $value)
+    {
+        $value = intval($value);
+        if ($value) {
+            $query->where($column, '>=', $value);
+        }
+    }
+
+    /**
+     * Max filter helper
+     *
+     * @param Builder $query
+     * @param string $column
+     * @param $value
+     */
+    protected function maxFilter(Builder $query, $column = '', $value)
+    {
+        $value = intval($value);
+        if ($value) {
+            $query->where($column, '<=', $value);
+        }
+    }
+
+    /**
+     * Sort helper
+     *
+     * @param Builder $query
+     * @param string $sortBy
+     * @param array $columnMap
+     */
+    protected function sort(Builder $query, $sortBy = '', $columnMap = [])
+    {
+        // Determine sort order by checking if the sort string begins with '-'
+        $order = $sortBy[0] === '-' ? 'desc' : 'asc';
+        // Remove the '-' from string
+        $sortBy = $sortBy[0] === '-' ? substr($sortBy, 1) : $sortBy;
+
+        if ($columnMap[$sortBy]) {
+            $query->orderBy($columnMap[$sortBy], $order);
+        }
+
+    }
+
 }
