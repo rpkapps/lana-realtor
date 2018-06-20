840ea4f1e8e8acad05cbc09ee8bdd8884632c7ef
Igor Kolesnik <ipkolesnikg@gmail.com>
2018-06-19 21:54:30 -0400

    Fix profile edit redirect

---
+++ app/Http/Controllers/Voyager/VoyagerBreadController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers\Voyager;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use TCG\Voyager\Events\BreadDataUpdated;
+use TCG\Voyager\Facades\Voyager;
+use TCG\Voyager\Http\Controllers\VoyagerBreadController as BaseVoyagerBreadController;
+
+class VoyagerBreadController extends BaseVoyagerBreadController
+{
+    public function update(Request $request, $id) {
+        if (!Auth::user()->hasPermission('edit_roles')) {
+            unset($request['role_id']);
+        }
+
+        $slug = $this->getSlug($request);
+
+        $dataType = Voyager::model('DataType')->where('slug', '=', $slug)->first();
+
+        // Compatibility with Model binding.
+        $id = $id instanceof Model ? $id->{$id->getKeyName()} : $id;
+
+        $data = call_user_func([$dataType->model_name, 'findOrFail'], $id);
+
+        // Check permission
+        $this->authorize('edit', $data);
+
+        // Validate fields with ajax
+        $val = $this->validateBread($request->all(), $dataType->editRows, $slug, $id);
+
+        if ($val->fails()) {
+            return response()->json(['errors' => $val->messages()]);
+        }
+
+        if (!$request->ajax()) {
+            $this->insertUpdateData($request, $slug, $dataType->editRows, $data);
+
+            event(new BreadDataUpdated($dataType, $data));
+
+            if($slug === 'users') {
+                return redirect()->back();
+            }
+
+            return redirect()
+                ->route("voyager.{$dataType->slug}.index")
+                ->with([
+                    'message'    => __('voyager.generic.successfully_updated')." {$dataType->display_name_singular}",
+                    'alert-type' => 'success',
+                ]);
+        }
+    }
+}
